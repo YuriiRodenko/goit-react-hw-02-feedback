eab0d90af549b6be380a5ec96d79dde94404f79b
YuriiRodenko <jury.rodenko@gmail.com>
2023-06-29 16:49:48 +0300

    step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,8 @@
-export const App = () => {
+import React, { Component } from 'react';
+
+
+
+export class App extends Component = () => {
   return (
     <div
       style={{
@@ -10,7 +14,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      homework template
     </div>
   );
 };
